cd4eaaf2115c4372c698a5bec27a21466267c25b
jaffar <116724930+jaffarcoding@users.noreply.github.com>
2023-01-29 14:58:30 +0530

    Update server.js

--- bekend/server.js
+++ bekend/server.js
@@ -16,7 +16,7 @@ process.on("uncaughtException", (err) => {
 
 // Config
 if (process.env.NODE_ENV !== "PRODUCTION") {
-  require("dotenv").config({ path: "backend/config/config.env" });
+  require("dotenv").config({ path: "bekend/config/config.env" });
 }
 
 connectDatabase();
